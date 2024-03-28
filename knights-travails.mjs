8d499604dd13321a46e665db095c45ac512dc3a5
tanat8350 <55195958+tanat8350@users.noreply.github.com>
2024-03-28 11:01:53 +0700

    finish knightMoves

--- knights-travails.mjs
+++ knights-travails.mjs
@@ -11,45 +11,44 @@ const possibleMoves = [
 
 function knightMoves(start, destination) {
   const destinationJson = JSON.stringify(destination);
-  const queue = [start];
-  const results = [];
+  const queue = [[start]];
   const already = [];
+  const shortest = [];
   let found = false;
   const move = (from, result = []) => {
     queue.shift();
 
     possibleMoves.forEach((item) => {
-      const first = from[0] + item[0];
-      const second = from[1] + item[1];
+      const first = from[from.length - 1][0] + item[0];
+      const second = from[from.length - 1][1] + item[1];
       const combined = [first, second];
 
       if (first >= 0 && first <= 7 && second >= 0 && second <= 7) {
         if (JSON.stringify(combined) === destinationJson) {
           found = true;
+          // push found shortest path
+          shortest.push(from.concat([combined]));
         }
         result.push(combined);
-        // if (queue[1]) {
-        //   result.push(queue[1].concat(from));
-        //   queue.shift();
-        // }
       }
     });
 
-    if (found === false)
+    if (!found)
       result.forEach((item) => {
         if (!already[item[0] * 10 + item[1]]) {
           already[item[0] * 10 + item[1]] = true;
-          queue.push(item);
+          queue.push(from.concat([item]));
         }
       });
-    return result;
   };
   while (queue.length >= 1) {
-    results.push(move(queue[0]));
-    // queue.shift();
+    move(queue[0]);
   }
-  return results;
+  const totalMoves = shortest[0].length - 1;
+  console.log(`You made it in ${totalMoves} moves!  Here's your path:`);
+  return shortest;
 }
 
-// console.log(knightMoves([0, 0], [1, 3]));
+console.log(knightMoves([0, 0], [4, 2]));
 console.log(knightMoves([7, 7], [1, 1]));
+console.log(knightMoves([7, 7], [0, 0]));
